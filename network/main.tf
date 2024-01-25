8cf27048b6fa60358bf84583655559a7c8beef0f
iamleet <rickalv.it@gmail.com>
2024-01-25 00:20:10 -0800

    Adding az to subnet and updating notes

--- network/main.tf
+++ network/main.tf
@@ -10,10 +10,11 @@ resource "aws_vpc" "dev" {
 }
 
 resource "aws_subnet" "dev" {
-    for_each    =   var.subnetworks
+    for_each                =   var.subnetworks
 
-    vpc_id      = aws_vpc.dev[each.value.vpc].id # Creates a dependency to the resouce above
-    cidr_block  = each.value.cidr_block
+    vpc_id                  = aws_vpc.dev[each.value.vpc].id # Creates a dependency to the resouce above
+    availability_zone_id    = each.value.az-id
+    cidr_block              = each.value.cidr_block
 
     tags = {
         name = each.value.tag
