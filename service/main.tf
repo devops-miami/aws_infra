729655997095cfd615276eb5ab4c83903fbb1109
iamleet <rickalv.it@gmail.com>
2024-01-25 23:15:41 -0800

    Example of link a var from resource to encrypted env

--- service/main.tf
+++ service/main.tf
@@ -21,7 +21,7 @@ resource "aws_cloudwatch_log_group" "default" {
     # The log group name format is /aws/eks/<cluster-name>/cluster
     # Reference: https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html
     name              = "/aws/eks/${each.key}/cluster"
-    retention_in_days = 2
+    retention_in_days = each.value.retention
 }
 
 ## EKS Cluster Stuffs 
