fa5836c96ea4119c236a617ae3c5dd015a9db3b0
iamleet <rickalv.it@gmail.com>
2024-01-28 02:01:39 -0800

    Sorting out some IAM and adding new role for nodepools

--- service/iam.tf
+++ service/iam.tf
@@ -15,20 +15,3 @@ resource "aws_iam_role_policy_attachment" "dev" {
     # Optionally, enable Security Groups for Pods
     # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
 }
-
-resource "aws_cloudwatch_log_group" "default" {
-    for_each = var.clusters
-    # The log group name format is /aws/eks/<cluster-name>/cluster
-    # Reference: https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html
-    name              = "/aws/eks/${each.key}/cluster"
-    retention_in_days = each.value.retention
-}
-
-resource "aws_eks_access_entry" "dev" {
-
-
-    cluster_name      = aws_eks_cluster.example.name
-    principal_arn     = aws_iam_role.example.arn
-    kubernetes_groups = ["group-1", "group-2"]
-      type              = "STANDARD"
-}
